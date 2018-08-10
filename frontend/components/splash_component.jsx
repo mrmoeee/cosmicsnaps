3c7dac565b713e8223ffb2d878b9d88b435e3be5
matt_moe <mrobertmoe@gmail.com>
2018-08-10 10:19:04 -0700

    Finish styling on splash page prep for demo user login

--- frontend/components/splash_component.jsx
+++ frontend/components/splash_component.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ImageComponent from './image_component';
+import { Link } from 'react-router-dom';
 
 const SplashComponent = () => {
 
@@ -7,9 +8,16 @@ const SplashComponent = () => {
     <div className="splash-container">
       <ImageComponent/>
 
-      <div className="h1-container">
-        <h1 className="h1-test">YOU CANT SEE ME!</h1>
-        <h2 className="h2-test">ME EITHER</h2>
+      <div className="text-block">
+        <div className="h1-container">
+          <h1 className="h1-test">Billions upon billions of stars, captured into a timeless picture</h1>
+        </div>
+        <div className="h2-container">
+          <h2 className="h2-test">Experience more than a pale blue dot</h2>
+        </div>
+        <div className="demo-container">
+          <Link className="demo" to="/signup">DEMO LOGIN</Link>
+        </div>
       </div>
     </div>
   );
