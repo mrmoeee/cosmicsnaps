f0784e8818259f06fc4939bac372862a6562a524
matt_moe <mrobertmoe@gmail.com>
2018-08-12 14:43:18 -0700

    Prepping for creating photos component

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,8 @@
 Rails.application.routes.draw do
-
   root "static_pages#root"
-
   namespace :api, defaults: {format: :json} do
     resources :users, only: [:create]
     resources :pictures, only: [:create, :index, :destroy, :show]
     resource :session, only: [:create, :destroy, :show]
   end
-
 end
