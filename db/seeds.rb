e34965cf9ab74c0d9f0a54225ff22c424853eda1
matt_moe <mrobertmoe@gmail.com>
2018-08-13 12:56:25 -0700

    Fix image path

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,9 +7,11 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 # ADD FIRST NAME AND LAST NAME TO FORM SHEET?
+User.destroy_all
+Picture.destroy_all
 User.create(username: "carlsagan", password: "123456");
 
 pic = Picture.create(title: 'This is an Astronaut', description: 'Floating through space', user_id: 6)
-pic.photo.attach(io: File.open('Users/mattmoe/Desktop/picdb/astro.jpg'), filename: 'astro.jpg')
+pic.photo.attach(io: File.open('./app/assets/images/astro.jpg'), filename: 'astro.jpg')
 #Picture.create
 #Picture.photo.attach(io: , filename:)
