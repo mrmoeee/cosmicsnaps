d5044cb5abd262166a2e0a1486fd575e9280cab0
matt_moe <mrobertmoe@gmail.com>
2018-08-15 13:07:18 -0700

    add one more pic to seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,8 +10,13 @@
 User.destroy_all
 Picture.destroy_all
 
-User.create(username: "carlsagan", password: "123456");
+user1 = User.create(username: "carlsagan", password: "123456");
+user2 = User.create(username: "suiladicastronaut", password: "astronaut");
 
-pic = Picture.new(title: 'This is an Astronaut', description: 'Floating through space', user_id: 1)
+pic = Picture.new(title: 'This is an Astronaut', description: 'Floating through space', user_id: user1.user_id)
 pic.photo.attach(io: File.open('./app/assets/images/astro.jpg'), filename: 'astro.jpg')
 pic.save!
+
+pic2 = Picture.new(title: 'This is an Astronaut', description: 'Floating through space', user_id: user2.user_id)
+pic2.photo.attach(io: File.open('./app/assets/images/earthstar.jpg'), filename: 'astro.jpg')
+pic2.save!
